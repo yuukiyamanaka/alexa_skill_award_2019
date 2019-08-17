06048a90cef72d0d19f150f55e66ddf057585fa4
Yuki Yamanaka <yuukiyamanaka13@gmail.com>
2019-08-17 15:03:36 +0900

    モジュール化

--- lambda/py/lambda_function.py
+++ lambda/py/lambda_function.py
@@ -11,8 +11,9 @@ from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.dispatch_components import AbstractExceptionHandler
 import ask_sdk_core.utils as ask_utils
 from ask_sdk_core.handler_input import HandlerInput
-
 from ask_sdk_model import Response
+
+from handlers import HelpIntentHandler, CancelOrStopIntentHandler, SessionEndedRequestHandler, IntentReflectorHandler, CatchAllExceptionHandler
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -165,112 +166,6 @@ class NoIntentHandler(AbstractRequestHandler):
         session_atr['level'] += 1
         
         return CounselingIntentHandler.handle(self, handler_input)
-
-class HelpIntentHandler(AbstractRequestHandler):
-    """
-    Alexa組み込みインテントHelpインテントが呼ばれた時のハンドラ
-    ex: 「アレクサ、＜＞でヘルプ」
-    """
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return ask_utils.is_intent_name("AMAZON.HelpIntent")(handler_input)
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        speak_output = "You can say hello to me! How can I help?"
-
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                .ask(speak_output)
-                .response
-        )
-
-class CancelOrStopIntentHandler(AbstractRequestHandler):
-    """
-    Alexa組み込みインテントのキャンセルインテントまたはストップインテントが呼ばれた時のハンドラ
-    ex: 「アレクサ、＜＞をやめる」
-    """
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return (ask_utils.is_intent_name("AMAZON.CancelIntent")(handler_input) or
-                ask_utils.is_intent_name("AMAZON.StopIntent")(handler_input))
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        speak_output = "ありがとうございました．お大事に!"
-
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                .response
-        )
-
-class SessionEndedRequestHandler(AbstractRequestHandler):
-    """
-    セッションを終了する際に呼ばれるハンドラ
-    クリーンアップする類の処理を行う
-    """
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return ask_utils.is_request_type("SessionEndedRequest")(handler_input)
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-
-        # Any cleanup logic goes here.
-
-        return handler_input.response_builder.response
-
-class IntentReflectorHandler(AbstractRequestHandler):
-    """
-    インテントが呼び出されたかを確認できるデバッグ用のハンドラ
-
-    The intent reflector is used for interaction model testing and debugging.
-    It will simply repeat the intent the user said. You can create custom handlers
-    for your intents by defining them above, then also adding them to the request
-    handler chain below.
-    """
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return ask_utils.is_request_type("IntentRequest")(handler_input)
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        intent_name = ask_utils.get_intent_name(handler_input)
-        speak_output = "You just triggered " + intent_name + "."
-
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                # .ask("add a reprompt if you want to keep the session open for the user to respond")
-                .response
-        )
-
-class CatchAllExceptionHandler(AbstractExceptionHandler):
-    """
-    エラーが発生した場合に呼び出されるハンドラ
-
-    Generic error handling to capture any syntax or routing errors. If you receive an error
-    stating the request handler chain is not found, you have not implemented a handler for
-    the intent being invoked or included it in the skill builder below.
-    """
-    def can_handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> bool
-        return True
-
-    def handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> Response
-        logger.error(exception, exc_info=True)
-
-        speak_output = "すみません。何かの問題が発生しました。最初からやり直してください。"
-
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                .ask(speak_output)
-                .response
-        )
 
 # The SkillBuilder object acts as the entry point for your skill, routing all request and response
 # payloads to the handlers above. Make sure any new handlers or interceptors you've
